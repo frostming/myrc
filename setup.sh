e64a1e77a7f3bbf2fe24db671c9f76932c04472d
Frost Ming <me@frostming.com>
2023-11-30 08:38:08 +0000

    setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -exo pipefail
+
+CURDIR=$(pwd)
+
+ln -sf $CURDIR/.zshrc ~/.zshrc
+ln -sf $CURDIR/.zimrc ~/.zimrc
+ln -sf $CURDIR/.gitconfig ~/.zimrc
+ln -sf $CURDIR/.p10k.zsh ~/.p10k.zsh
+
+[ ! -d $HOME/.ssh ] && mkdir -p $HOME/.ssh
+cp $CURDIR/git_sign.pub $HOME/.ssh/
+
